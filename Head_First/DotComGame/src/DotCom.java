0ed434d588cb0b8900ef0eb853f76733c6c7ea03
syshim77 <syshim77@gmail.com>
2017-09-06 15:15:13 +0900

    self study project

---
+++ Head_First/DotComGame/src/DotCom.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class DotCom {
+	private ArrayList<String> locationCells;
+	private String name;
+	
+	public void setLocationCells(ArrayList loc) {
+		locationCells = loc;
+	}
+	
+	public void setName(String n) {
+		name = n;
+	}
+	
+	public String checkYourself(String userInput) {
+		String status = "miss";
+		int index = locationCells.indexOf(userInput);
+		
+		if (index>=0) {
+			locationCells.remove(index);
+			
+			if (locationCells.isEmpty()) {
+				status = "kill";
+				System.out.println("Ouch! You sunk " + name + "   :)");
+			} else {
+				status = "hit";
+			}
+		}
+		
+		return status;
+	}
+}
